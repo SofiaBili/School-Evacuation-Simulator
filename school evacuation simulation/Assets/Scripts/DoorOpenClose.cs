8e1f290446b25847fe2d4cd8b1a78f674620ad5b
SofiaBili <sofia.bili@yahoo.gr>
2023-03-25 23:15:56 +0200

    doors and fix books

--- school evacuation simulation/Assets/Scripts/DoorOpenClose.cs
+++ school evacuation simulation/Assets/Scripts/DoorOpenClose.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class DoorOpenClose : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    DoorOpenClose doorOpenClose;
+    [SerializeField] GameObject doorHitbox;
+    [SerializeField] Transform door;
+    [SerializeField] float doorSpeed = 35f;
+    bool flag=false;
+
+    void Awake(){
+        doorOpenClose = doorHitbox.GetComponent<DoorOpenClose>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnTriggerStay(Collider other){
+        if(other.gameObject.CompareTag("Player")){
+            Debug.Log(door.transform.localRotation.eulerAngles.z);
+            if (Input.GetKeyDown("space")){
+                flag = true;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other){
+    }
+
+    void Update(){
+        if(flag){
+            if(door.transform.localRotation.eulerAngles.z<115){
+                door.Rotate(0, 0, (doorSpeed * Time.deltaTime));
+            }
+        }
     }
 }
