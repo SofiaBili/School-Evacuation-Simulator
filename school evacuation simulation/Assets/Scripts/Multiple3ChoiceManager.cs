34a731a3c6e2f22ed89f4a956ed1a1894cae5791
SofiaBili <sofia.bili@yahoo.gr>
2023-04-17 00:26:52 +0300

    fixed last questions

--- school evacuation simulation/Assets/Scripts/Multiple3ChoiceManager.cs
+++ school evacuation simulation/Assets/Scripts/Multiple3ChoiceManager.cs
@@ -15,6 +15,27 @@ public class Multiple3ChoiceManager : MonoBehaviour
 	[SerializeField] private TextMeshProUGUI ans1Text;
 	[SerializeField] private TextMeshProUGUI ans2Text;
 
+    StartQuestionProcedureScript startQuestionProcedureScript;
+    GameObject startQuestionProcedureScriptObject;
+    ToggleQuestionCanvas toggleQuestionCanvasScript;
+    [SerializeField] GameObject toggleQuestionCanvasObject;
+
+	[SerializeField] Button but1;
+	[SerializeField] Button but2;
+	[SerializeField] Button but3;
+	static int randomQuestionIndex;
+
+	
+	[SerializeField] Camera canvasCamera;
+	
+    [SerializeField] GameObject slimeObject;
+	Animator slimeAnimator;
+    private string currentState;
+
+	void Awake(){
+		toggleQuestionCanvasScript = toggleQuestionCanvasObject.GetComponent<ToggleQuestionCanvas>();
+        slimeAnimator = slimeObject.GetComponent<Animator>();
+	}
 	void Start(){
 		if(unansweredQuestions == null || unansweredQuestions.Count==0){
 			unansweredQuestions = questions.ToList<Multiple3ChoiceQuestios>();
@@ -31,31 +52,80 @@ public class Multiple3ChoiceManager : MonoBehaviour
 		ans2Text.text = currentQuestion.ans3;
 		unansweredQuestions.RemoveAt(randomQuestionIndex);
 	}
+    private IEnumerator ChangeButtonColour(Button btn){
+		yield return new WaitForSeconds (1.9f);
+		btn.GetComponent<Image>().color = Color.white;
+	}
 	
+    private IEnumerator ChangeQuestion(){
+		yield return new WaitForSeconds (2.1f);
+		canvasCamera.cullingMask &=  ~(1 << LayerMask.NameToLayer("QuestionCanvas"));
+		//canvasCamera.cullingMask |=  (1 << LayerMask.NameToLayer("QuestionCanvas"));
+		SetCurrentQuestion();
+		yield return new WaitForSeconds (0.09f);
+		//canvasCamera.cullingMask |=  (1 << LayerMask.NameToLayer("QuestionCanvas"));
+		//canvasCamera.cullingMask &=  ~(1 << LayerMask.NameToLayer("QuestionCanvas"));
+		Debug.Log(unansweredQuestions.Count);
+	}
 	public void UserSelectExit(){
-		//StartQuestionProcedureScript.StopAnimationAndCloseCanvas();
+		startQuestionProcedureScript = toggleQuestionCanvasScript.GetCurrHitbox().GetComponent<StartQuestionProcedureScript>();
+		startQuestionProcedureScript.StopAnimationAndCloseCanvasFromExit();
+		StartCoroutine(ChangeQuestion());
 	}
 	public void UserSelect0(){
+		startQuestionProcedureScript = toggleQuestionCanvasScript.GetCurrHitbox().GetComponent<StartQuestionProcedureScript>();
 		if(currentQuestion.corrAns == 0){
 			Debug.Log("Cor");
+			but1.GetComponent<Image>().color = Color.green;
+			startQuestionProcedureScript.DeleteHexagon();
+			unansweredQuestions.RemoveAt(randomQuestionIndex);
+			ChangeAnimationState("congratulations");
 		}else{
+			but1.GetComponent<Image>().color = Color.red;
+			startQuestionProcedureScript.StopAnimationAndCloseCanvas();
 			Debug.Log("WRONG");
+			ChangeAnimationState("disappoint");
 		}
+		StartCoroutine(ChangeButtonColour(but1));
+		StartCoroutine(ChangeQuestion());
 	}
-	
 	public void UserSelect1(){
+		startQuestionProcedureScript = toggleQuestionCanvasScript.GetCurrHitbox().GetComponent<StartQuestionProcedureScript>();
 		if(currentQuestion.corrAns == 1){
 			Debug.Log("Cor");
+			but2.GetComponent<Image>().color = Color.green;
+			startQuestionProcedureScript.DeleteHexagon();
+			unansweredQuestions.RemoveAt(randomQuestionIndex);
+			ChangeAnimationState("congratulations");
 		}else{
+			but2.GetComponent<Image>().color = Color.red;
+			startQuestionProcedureScript.StopAnimationAndCloseCanvas();
 			Debug.Log("WRONG");
+			ChangeAnimationState("disappoint");
 		}
+		StartCoroutine(ChangeButtonColour(but2));
+		StartCoroutine(ChangeQuestion());
 	}
-	
 	public void UserSelect2(){
+		startQuestionProcedureScript = toggleQuestionCanvasScript.GetCurrHitbox().GetComponent<StartQuestionProcedureScript>();
 		if(currentQuestion.corrAns == 2){
 			Debug.Log("Cor");
+			but3.GetComponent<Image>().color = Color.green;
+			startQuestionProcedureScript.DeleteHexagon();
+			unansweredQuestions.RemoveAt(randomQuestionIndex);
+			ChangeAnimationState("congratulations");
 		}else{
+			but3.GetComponent<Image>().color = Color.red;
+			startQuestionProcedureScript.StopAnimationAndCloseCanvas();
 			Debug.Log("WRONG");
+			ChangeAnimationState("disappoint");
 		}
+		StartCoroutine(ChangeButtonColour(but3));
+		StartCoroutine(ChangeQuestion());
 	}
+	public void ChangeAnimationState(string newState){
+        currentState=newState;
+        if(!slimeAnimator.GetCurrentAnimatorStateInfo(0).IsName(currentState))
+            slimeAnimator.Play(newState);
+    }
 }
