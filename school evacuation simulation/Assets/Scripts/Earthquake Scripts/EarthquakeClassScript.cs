4e52fd8435f86ca984ab71e69fcf9e6498e8e0c5
SofiaBili <sofia.bili@yahoo.gr>
2023-05-30 19:42:24 +0300

    start earthquake

--- school evacuation simulation/Assets/Scripts/Earthquake Scripts/EarthquakeClassScript.cs
+++ school evacuation simulation/Assets/Scripts/Earthquake Scripts/EarthquakeClassScript.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 
 public class EarthquakeClassScript : MonoBehaviour
 {
-    public Animator earthquakeAnimation;
+    public Animator earthquakeAnimator;
 	public GameObject hitbox1, hitbox2, hitbox3, hitbox4;
-
-
+	public GameObject floorHitBox;
+	public GameObject dustParticles;
+	public static bool worriedTeacher = false;
+    private string currentState;
     // Start is called before the first frame update
-    void Start()
-    {
-		StartCoroutine(StartEarthquake());
+    void Start(){
+        earthquakeAnimator = GetComponent<Animator>();
     }
+    private void OnTriggerStay(Collider other){
+        if(other.gameObject.CompareTag("Player")){
+			Debug.Log("ppppppppp");
+			StartCoroutine(StartEarthquake());
+		}
+	}
 	public IEnumerator StartEarthquake(){
 		if(EarthquakeGuideScript.guideIsOver){
         	yield return new WaitForSeconds (10f);
-			earthquakeAnimation.Play(0);
+			ChangeAnimationState("earthquake");
+			worriedTeacher = true;
+        	yield return new WaitForSeconds (5f);
+			dustParticles.SetActive(true);
 		}
 	}
+	public void ChangeAnimationState(string newState){
+        currentState=newState;
+        if(!earthquakeAnimator.GetCurrentAnimatorStateInfo(0).IsName(currentState))
+            earthquakeAnimator.Play(newState);
+    }
 }
