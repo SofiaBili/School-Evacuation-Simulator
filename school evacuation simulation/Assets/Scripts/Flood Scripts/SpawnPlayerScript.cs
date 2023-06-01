f54a00404554cc7df75c743864536d74ec9874e1
LimnosJason <limnosjason@gmail.com>
2023-06-01 11:30:43 +0300

    Flood Spawn Point

---
+++ school evacuation simulation/Assets/Scripts/Flood Scripts/SpawnPlayerScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlayerScript : MonoBehaviour
+{
+
+    [SerializeField] GameObject playerPrefab;
+    [SerializeField] GameObject mapGameObject;
+
+    List<Transform> spawns = new List<Transform>();
+    GameObject instantiatedObject;
+    // Start is called before the first frame update
+    void Start() {
+        FindAllSpawns();
+    }
+
+    void FindAllSpawns(){
+        foreach(Transform thing in mapGameObject.GetComponentsInChildren<Transform>()){
+            //Debug.Log(thing.name);
+            if(thing.name=="Spawn Point"){
+                spawns.Add(thing);
+                //Debug.Log(spawns);
+            }
+        }
+        SpawnAtRandomSpawnPoint();
+    }
+
+    void SpawnAtRandomSpawnPoint(){
+        int randomPos;
+        if(spawns.Count>0){
+            randomPos = Random.Range(0, spawns.Count);
+            //Debug.Log(spawns.Count);
+            //Debug.Log(randomPos);
+            instantiatedObject=Instantiate(playerPrefab);
+            instantiatedObject.name = "Player";
+            instantiatedObject.transform.SetParent(spawns[randomPos].transform.parent);
+            instantiatedObject.transform.position = spawns[randomPos].transform.position;
+            //instantiatedObject.GetComponent<LookAt>().enabled=false;
+            spawns.RemoveAt(randomPos);
+        }
+        playerPrefab.SetActive(false);
+    }
+}
