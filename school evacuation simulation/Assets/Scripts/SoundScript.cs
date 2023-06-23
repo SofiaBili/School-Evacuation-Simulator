20b304f649d5d37afb051af344a3bebe0857de36
SofiaBili <sofia.bili@yahoo.gr>
2023-06-23 17:33:42 +0300

    settings

--- school evacuation simulation/Assets/Scripts/SoundScript.cs
+++ school evacuation simulation/Assets/Scripts/SoundScript.cs
@@ -6,31 +6,18 @@ using UnityEngine.Audio;
 
 public class SoundScript : MonoBehaviour
 {
-    [SerializeField] Slider musicSlider;
     [SerializeField] Slider sfxSlider;
     [SerializeField] AudioMixer mixer;
 
     void Start(){
-        musicSlider.value=PlayerPrefs.GetFloat("MusicVolume");
         sfxSlider.value=PlayerPrefs.GetFloat("SfxVolume");
-        ChangeMixerMusic();
         ChangeMixerSfx();
-    }
-
-    public void ChangeMusicVolume(){
-        PlayerPrefs.SetFloat("MusicVolume",musicSlider.value);
-        ChangeMixerMusic();
-        PlayerPrefs.Save();
     }
 
     public void ChangeSfxVolume(){
         PlayerPrefs.SetFloat("SfxVolume",sfxSlider.value);
         ChangeMixerSfx();
         PlayerPrefs.Save();
-    }
-
-    private void ChangeMixerMusic(){
-        mixer.SetFloat("Music",musicSlider.value);
     }
 
     private void ChangeMixerSfx(){
